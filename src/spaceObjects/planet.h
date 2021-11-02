1211d2ff8a1e178e270247e5dffc8ee6aaf97f3d
gcask <53709079+gcask@users.noreply.github.com>
2021-11-02 03:45:11 -0400

    Planet tweaks (#1590)
    * Tweaks to planet API
    
    * Update planet.cpp
    
    * atmosphere_color is now a vec3

--- src/spaceObjects/planet.h
+++ src/spaceObjects/planet.h
@@ -27,11 +27,11 @@ public:
     float getCollisionSize();
 
     void setPlanetAtmosphereColor(float r, float g, float b);
-    void setPlanetAtmosphereTexture(string texture_name);
-    void setPlanetSurfaceTexture(string texture_name);
+    void setPlanetAtmosphereTexture(std::string_view texture_name);
+    void setPlanetSurfaceTexture(std::string_view texture_name);
     string getPlanetSurfaceTexture();
-    void setPlanetIcon(string texture_name);
-    void setPlanetCloudTexture(string texture_name);
+    void setPlanetIcon(string texture_name); //TODO : check string view
+    void setPlanetCloudTexture(std::string_view texture_name);
     void setPlanetRadius(float size);
     void setPlanetCloudRadius(float size);
     void setPlanetAtmosphereRadius(float size);
@@ -54,7 +54,7 @@ private:
     string planet_icon;
     string cloud_texture;
     string atmosphere_texture;
-    glm::u8vec4 atmosphere_color;
+    glm::vec3 atmosphere_color{};
     float distance_from_movement_plane;
 
     float axial_rotation_time;
